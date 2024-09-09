0fc313f896d39ff02d9ae3a27f155c10d883a1f9
Filip <143486317+FilipBjorkman@users.noreply.github.com>
2024-09-09 15:51:02 +0200

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,6 +1,7 @@
----
-const x = 5
+const x = 5;
 
-function double (x) { return x * 2 }
+function double(x) {
+  return x * 2
+}
 
-console.print('Dubbelt av 5 är ' + double(x))
+console.log('Dubbelt av 5 är ' + double(x))
